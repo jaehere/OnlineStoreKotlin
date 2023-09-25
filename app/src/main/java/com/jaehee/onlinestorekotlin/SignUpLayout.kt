cf11c223198d6dae36fe8f7d04414cda0c4d7f34
jaeheelee <0jaehui@gmail.com>
2023-09-25 13:37:29 +0900

    Initial commit

---
+++ app/src/main/java/com/jaehee/onlinestorekotlin/SignUpLayout.kt
@@ -0,0 +1,11 @@
+package com.jaehee.onlinestorekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_layout)
+    }
+}
