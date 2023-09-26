1b4d95638b3347c47c5b805cdc476e5a2ea7df0b
jaeheelee <0jaehui@gmail.com>
2023-09-26 16:38:50 +0900

    Registration Process

--- app/src/main/java/com/jaehee/onlinestorekotlin/MainActivity.kt
+++ app/src/main/java/com/jaehee/onlinestorekotlin/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.jaehee.onlinestorekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jaehee.onlinestorekotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.activityMainBtnSignUp.setOnClickListener {
+
+            var signUpIntent = Intent(this@MainActivity, SignUpLayout::class.java)
+            startActivity(signUpIntent)
+
+        }
+
     }
 }
--- app/src/main/java/com/jaehee/onlinestorekotlin/SignUpLayout.kt
+++ app/src/main/java/com/jaehee/onlinestorekotlin/SignUpLayout.kt
@@ -2,10 +2,67 @@ package com.jaehee.onlinestorekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.jaehee.onlinestorekotlin.databinding.SignUpLayoutBinding
+
 
 class SignUpLayout : AppCompatActivity() {
+    private lateinit var binding: SignUpLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up_layout)
+        binding = SignUpLayoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.signUpLayoutBtnSignUp.setOnClickListener {
+            if(binding.signUpLayoutEdtPassword.text.toString().equals(
+                    binding.singUpLayoutEdtConfirmPassword.text.toString()
+            )){
+                // Registration Process
+
+                val signUpURL = "http://61.109.169.174//OnlineStoreAPP/join_new_user.php?" +
+                        "email=${binding.signUpLayoutEdtEmail.text.toString()}" +
+                        "&username=${binding.signUpLayoutEdtUsername.text.toString()}" +
+                        "&pass=${binding.signUpLayoutEdtPassword.text.toString()}"
+
+                val requestQ = Volley.newRequestQueue(this@SignUpLayout)
+                val stringRequest = StringRequest(Request.Method.GET, signUpURL, Response.Listener {
+                    response ->
+
+                    if(response.equals("A user with this Email Address already exists")){
+
+                        val dialogBuilder = AlertDialog.Builder(this)
+                        dialogBuilder.setTitle("Message")
+                        dialogBuilder.setMessage(response)
+                        dialogBuilder.create().show()
+                    }
+
+                }, Response.ErrorListener {
+                    error ->
+
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setTitle("Message")
+                    dialogBuilder.setMessage(error.message)
+                    dialogBuilder.create().show()
+                })
+
+                requestQ.add(stringRequest)
+
+
+            }else{
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message")
+                dialogBuilder.setMessage("Password Mismatch")
+                dialogBuilder.create().show()
+
+            }
+        }
+
+
+
     }
 }
